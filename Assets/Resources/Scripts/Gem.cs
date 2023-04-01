aa5757b2d7ee7879ad8ffb48ab99b1b6bff01713
ngoc vo <ngocduyvo12@gmail.com>
2023-04-01 02:30:13 -0600

    push to github

---
+++ Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// despawn coin if it goes past the left edge of the screen
+		if (transform.position.x < -25) {
+			Destroy(gameObject);
+		}
+		else {
+
+			// ensure gem moves at the same rate as the skyscrapers do
+			transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+		}
+
+		// infinitely rotate this gem about the Y axis in world space
+		transform.Rotate(0, 5f, 0, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other) {
+		other.transform.parent.GetComponent<HeliController>().PickupGem();
+		Destroy(gameObject);
+	}
+}
